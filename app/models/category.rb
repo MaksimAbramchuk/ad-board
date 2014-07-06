1c50bb124356cabbb618c4a9efb23e1a4ba38194
Maxim Abramchuck <maximabramchuck@gmail.com>
2014-07-06 22:58:50 +0300

    add dependencies advert-images advert-category on destroy, closed #16

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true
   has_many :adverts
+
+  before_destroy :check_adverts
+
+  protected
+
+  def check_adverts
+    return false unless self.adverts.empty?
+  end
+
 end
