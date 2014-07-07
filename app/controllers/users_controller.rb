f6340c117c5452057c60dfa811f7688c6a4b1d36
Maxim Abramchuck <maximabramchuck@gmail.com>
2014-07-07 10:42:45 +0300

    cleaned routes.rb, fixed #17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def all
+  def index
     @users = User.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'adverts#index'
 
+  get 'account', to: 'accounts#index'
   get 'account/role', to: 'accounts#role'
   post 'account/role/change', to: 'accounts#change_role'
-  get 'account', to: 'accounts#index'
-  get 'adverts/awaiting_publication', to: 'adverts#awaiting_publication'
-  get 'users/all', to: 'users#all'
-  get 'users/:id/', to: 'users#show', as: 'user'
   get 'account/adverts/', to: 'accounts#adverts'
-  post 'adverts/:id/change', to: 'comments#creater'
-  get 'adverts/:id/logs/', to: 'adverts#logs', as: 'advert_logs'
+
   get '/search/result', to: 'search#result'
 
   resources :adverts do
     get 'change'
     match 'change' => 'adverts#change_state', via: [:put, :patch]
+    get 'awaiting_publication', on: :collection
+    get 'logs'
   end
 
+  resources :users, only: [:index, :show]
   resources :categories
+
 end
