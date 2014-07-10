7a5b4c83185b300d5acc3ccd5ae9811ce97cef11
Maxim Abramchuck <maximabramchuck@gmail.com>
2014-07-10 21:56:32 +0300

    change edit form, add thumbnails, closed #14

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,6 @@ class Image < ActiveRecord::Base
 
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, styles: { small: '150×150', large: '320×240' }
+  has_attached_file :image, styles: { small: '100×100', large: '320×240' }
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'], default_url: '/images/:style/missing.png'
 end
